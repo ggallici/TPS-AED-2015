f95e22b826978c15f251d5f9550c56871251d37e
arielLiserre <a.liserre@yahoo.com>
2015-07-01 12:47:45 -0300

    Update Fibonacci.cpp

--- TP9/Fibonacci.cpp
+++ TP9/Fibonacci.cpp
@@ -51,12 +51,14 @@ long long fib (int)
 unsigned fibonacci (int n)
 {
 	static array <unsigned, 21> f={0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765}
-if(n<22) return f.at(m);
+if(n<22) return f.at(n);
 return fib(n-1) + fib(n-2);
 }
 
 int main ()
 {
+	int n;
+	
 	cout << "fib";
 	
 	cout << "fibonacci";
