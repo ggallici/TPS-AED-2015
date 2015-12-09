aa8db1d68cd37b3653c5bb7cebcdbb51d819ead9
German Gallici <gallicig94@gmail.com>
2015-12-09 09:26:34 -0300

    Modificacion de notacion

--- TP13/TP13.cpp
+++ TP13/TP13.cpp
@@ -1,65 +1,72 @@
 #include <iostream>
 #include<stdlib.h>
+#include <array>
 
 using namespace std;
 
 int main()
 {
-    int matriz [2][3],i,j,n;
-    int suma=0,vend=0,reg=0,mven=0,mreg=0;
+    array<array<int, 3>,2> matriz;
+    int suma = 0, vend = 0, reg = 0, mven = 0, mreg = 0;
 
-
-    for (i=0; i<=1; i++){
-        for (j=0; j<=2; j++){
-            cout<<"Digite Posicion"<<" "<<i<<"."<<j<<": ";
-            cin>>n;
-            matriz [i][j]=n;
+    for (unsigned i = 0; i < matriz.size(); i++)
+    {
+        for (unsigned j = 0; j < matriz.at(i).size(); j++)
+        {
+            int n;
+            cout << "Digite Posicion " << i << "." << j << ": ";
+            cin >> n;
+            matriz.at(i).at(j) = n;
         }
     }
 
     system ("cls");
 
-
-    for (i=0; i<=1; i++){
-        for (j=0; j<=2; j++){
-            cout<<matriz [i][j]<<" ";
-
+    for (unsigned i = 0; i < matriz.size(); i++)
+    {
+        for (unsigned j = 0; j < matriz.at(i).size(); j++)
+        {
+            cout << matriz.at(i).at(j) << " ";
         }
-        cout<<"\n";
+        cout << endl;
     }
 
-     for (i=0; i<=1; i++){
-        for (j=0; j<=2; j++){
-            suma=suma+matriz [i][j];
-            vend=vend+matriz [i][j];
+    cout << endl;
 
+    for (unsigned i = 0; i < matriz.size(); i++)
+    {
+        for (unsigned j = 0; j < matriz.at(i).size(); j++)
+        {
+            suma = suma + matriz.at(i).at(j);
+            vend = vend + matriz.at(i).at(j);
         }
-    if (mven<vend){
-        mven=vend;
+        if (mven < vend)
+        {
+            mven = vend;
+        }
+
+        cout << "Vendedor " << i + 1 << ": $" << vend << endl;
+        vend = 0;
     }
-    cout<<"\n"<<"Vendedor "<<i+1<<": $"<<vend;
-    vend=0;
+
+    cout << "Mayor Venta Vendedor: $" << mven << endl << endl;
+
+    for (unsigned j = 0; j < matriz.at(0).size(); j++)
+    {
+        for (unsigned i = 0; i < matriz.size(); i++)
+        {
+            reg = reg + matriz.at(i).at(j);
         }
-
-
-cout<<"\n"<<"Ventas Totales: $"<<suma;
-cout<<"\n"<<"Mayor Venta Vendedor: $"<<mven;
-
-
-     for (j=0; j<=2; j++){
-        for (i=0; i<=1; i++){
-            reg=reg+matriz [i][j];
-
+        if (mreg < reg)
+        {
+            mreg = reg;
         }
-    if (mreg<reg){
-        mreg=reg;
+        cout << "Region " << j + 1 << ": $" << reg << endl;
+        reg = 0;
     }
-    cout<<"\n"<<"Region "<<j+1<<": $"<<reg;
-    reg=0;
-        }
 
+    cout << "Mayor Venta Region: $" << mreg << endl;
 
-cout<<"\n"<<"Mayor Venta Region: $"<<mreg;
-    return 0;
+    cout << endl
+         <<  "Ventas Totales: $" << suma << endl;
 }
-
